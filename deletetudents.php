e7f4d476640987851e5631dfaacd075911a1b1fe
Tran Anh Van <anhvandz02@gmail.com>
2022-11-26 00:14:26 +0700

    delete_file_not_importace_2

--- deletetudents.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connect.php';
-$userid = $_GET['userid'];
-$sql = "DELETE FROM `user` WHERE userid = $userid";
-$result = mysqli_query($conn, $sql);
-mysqli_close($conn);
-header('location:students.php');
-?>
